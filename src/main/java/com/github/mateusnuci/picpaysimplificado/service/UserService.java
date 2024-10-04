6ad4b09bed228b68425192a10feeec3ffb961269
Mateus Nuci <mateusnucicosta@gmail.com>
2024-10-04 16:37:08 -0300

    feat: create UserService

---
+++ src/main/java/com/github/mateusnuci/picpaysimplificado/service/UserService.java
@@ -0,0 +1,37 @@
+package com.github.mateusnuci.picpaysimplificado.service;
+
+import com.github.mateusnuci.picpaysimplificado.domain.user.User;
+import com.github.mateusnuci.picpaysimplificado.domain.user.UserType;
+import com.github.mateusnuci.picpaysimplificado.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT)
+            throw new Exception("Usuário não tem autorização para realizar transações");
+        if(sender.getBalance().compareTo(amount) < 0)
+            throw new Exception("Saldo insuficiente");
+    }
+
+    public User findUserById(Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent())
+            return user.get();
+        throw new Exception("Usuário não encontrado");
+    }
+
+    public void insertUser(User user) {
+        userRepository.save(user);
+    }
+
+
+}
