a3ca853a7bf1e9df68d264d5fe117ad491f908fd
Mateus Nuci <mateusnucicosta@gmail.com>
2024-10-07 17:52:46 -0300

    feat: Notification layers added

---
+++ src/main/java/com/github/mateusnuci/picpaysimplificado/dto/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.github.mateusnuci.picpaysimplificado.dto;
+
+public record NotificationDTO(String email, String message) {
+}
---
+++ src/main/java/com/github/mateusnuci/picpaysimplificado/service/NotificationService.java
@@ -0,0 +1,26 @@
+package com.github.mateusnuci.picpaysimplificado.service;
+
+import com.github.mateusnuci.picpaysimplificado.domain.user.User;
+import com.github.mateusnuci.picpaysimplificado.dto.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws  Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            throw new Exception("Serviço de norificaçlão fora do ar");
+        }
+    }
+}
