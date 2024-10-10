ae3edb0125ba8b96f88786fff67faf27bc83d3fb
Mateus Nuci <mateusnucicosta@gmail.com>
2024-10-10 14:58:48 -0300

    v 1.0

--- src/main/java/com/github/mateusnuci/picpaysimplificado/PicpaysimplificadoApplication.java
+++ src/main/java/com/github/mateusnuci/picpaysimplificado/PicpaysimplificadoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PicpaysimplificadoApplication {
 
+	// V1
 	public static void main(String[] args) {
 		SpringApplication.run(PicpaysimplificadoApplication.class, args);
 	}
